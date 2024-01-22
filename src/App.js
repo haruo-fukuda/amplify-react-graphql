efb31a7fb2f9f87a4a042667f18986778045dc16
Haruo.Fukuda <haruo.fukuda@access-company.com>
2024-01-22 12:09:41 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
 
 export default App;
 */
+/*
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -41,3 +42,28 @@ function App() {
 }
 
 export default App;
+*/
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
